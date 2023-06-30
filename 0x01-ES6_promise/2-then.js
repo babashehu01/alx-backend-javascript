0b2dff011949cc4b116d0ec8909eb34b6e64dceb
babashehu01 <shettimababashehu57@gmail.com>
2023-06-30 00:33:45 +0000

    testing

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -2,7 +2,7 @@ export default function handleResponseFromAPI(promise) {
   const data = { status: 200, body: 'success' };
   promise
     .then(() => (data))
-    .catch(() => { Error(''); })
+    .catch(() => { Error(' '); })
     .finally(() => {
       console.log('Got a response from the API');
     });
