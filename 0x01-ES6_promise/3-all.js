9ad932d661db1898219696cc5769e0121679128c
babashehu01 <shettimababashehu57@gmail.com>
2023-06-30 01:51:42 +0000

    Adding

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -4,7 +4,7 @@ export default function handleProfileSignup() {
   Promise.all([uploadPhoto(), createUser()])
     .then(results => {
       console.log(`${results[0].body} ${results[1].firstName} ${results[1].lastName}`);
-    })
+    });
     .catch(error => {
       console.error('Signup system offline');
     });
