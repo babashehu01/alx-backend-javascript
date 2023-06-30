b96c74038e2f6532a21ff75c00359336ee22eef3
babashehu01 <shettimababashehu57@gmail.com>
2023-06-30 01:44:50 +0000

    Adding

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,7 +3,7 @@ import { uploadPhoto, createUser } from './utils';
 export default function handleProfileSignup() {
   Promise.all([uploadPhoto(), createUser()])
     .then(results => {
-      console.log(`${results[0].body} ${result[1].createUserResult.firstName} ${result[2].createUserResult.lastName}`);
+      console.log(`${results[0].body} ${results[1].createUserResult.firstName} ${results[2].createUserResult.lastName}`);
     })
     .catch(error => {
       console.error('Signup system offline');
